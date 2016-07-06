1c14d41f9fa5a43ee6e364dbd49fb1d146d4327b
MasterSamurai <thomas@contis.org>
2016-07-06 10:06:10 -0400

    Update ProjectEulerProblem1.py

--- Project_Euler/ProjectEulerProblem1.py
+++ Project_Euler/ProjectEulerProblem1.py
@@ -1,7 +1,4 @@
 # Problem 1
 """Find the sum of all the multiples of 3 or 5 below 1000."""
-a = 0
-for i in range(1000):
-    if i % 3 == 0 or i % 5 == 0:
-        a += i
-print(a)
+def find_sum_of_multiples(limit, *multiples):
+    return sum([i for i in range(limit) if any(i % j == 0 for j in multiples)])
