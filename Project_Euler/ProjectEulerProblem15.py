0ee9fd3b15b75adb73849f667cc75e0099458dd2
MasterSamurai <thomas@contis.org>
2016-07-10 12:41:35 -0400

    Update ProjectEulerProblem15.py

--- Project_Euler/ProjectEulerProblem15.py
+++ Project_Euler/ProjectEulerProblem15.py
@@ -1,10 +1,12 @@
 # Problem 15:
 """Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down,
 there are exactly 6 routes to the bottom right corner. How many such routes are there through a 20×20 grid?"""
-def find_paths(grid_size):
-    paths = 1
-    for i in range(0,grid_size):
-        paths *= (2*grid_size - i)
-        paths //= (i + 1)
-    return paths
-print(find_paths(20))
+def factorial(number):
+    new = 1
+    for i in range(number,1,-1):
+        new *= i
+    return new
+
+def find_paths(grid_height, grid_width):
+    return (factorial(grid_height+grid_width)//factorial(grid_width))//factorial(grid_height)
+print(find_paths(20,20))
