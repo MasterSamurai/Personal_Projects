8eedb9a07664bc33463e303e8131fec7fa3b2155
MasterSamurai <thomas@contis.org>
2016-07-11 22:21:15 -0400

    Create ProjectEulerProblem25.py

---
+++ Project_Euler/ProjectEulerProblem25.py
@@ -0,0 +1,10 @@
+# Problem 25:
+"""What is the index of the first term in the Fibonacci sequence to contain 1000 digits?"""
+def nth_Digit_Fibbonacci(num_digits):
+    a,b,i,l = 1,1,2,0
+    while l < num_digits:
+        i += 1
+        a,b = b,a+b
+        l = len(str(b))
+
+    return i
