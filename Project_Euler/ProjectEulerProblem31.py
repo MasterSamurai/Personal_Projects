adac5d7dff08913b3a6587b9412f7561fdb05036
Thomas Contis <thomas@contis.org>
2016-07-19 15:13:37 -0400

    Create ProjectEulerProblem31.py

---
+++ Project_Euler/ProjectEulerProblem31.py
@@ -0,0 +1,11 @@
+# Problem 31:
+"""How many different ways can £2 be made using any number of coins?"""
+coins = [1,2,5,10,20,50,100,200]
+
+def diff_Combinations_Coins(coin_denominations, target):
+    c = [1] + [0]*target
+    for coin in coin_denominations:
+        for i in range(coin,target+1):
+            c[i] += c[i-coin]
+    return c[target]
+print(diff_Combinations_Coins(coins, 200))
